84a62972ca629447aff4fb8c3349b5a0052ccc45
Shubham Yadav <yadavshubham6.022@gmail.com>
2025-10-09 22:03:53 +0530

    Hero page is created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import CarDetails from './pages/CarDetails';
+import Cars from './pages/Cars';
+import MyBookings from './pages/MyBookings';  
+
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith('/owner');
   return (
     <>
-      <Navbar />
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/car-details/:id' element={<CarDetails />} />
+        <Route path='/cars' element={<Cars />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+      </Routes>
     </>
   )
 }
