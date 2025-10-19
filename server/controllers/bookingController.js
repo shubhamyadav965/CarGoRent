a126d5e0027bf74a897f5a432af82f6cfdf6b973
Shubham Yadav <yadavshubham6.022@gmail.com>
2025-10-19 23:04:11 +0530

    update booking status

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -76,3 +76,36 @@ export const getUserBookings = async (req, res) => {
         res.json({ success: false, message: error.message });
     }
 }
+
+// API to get owner bookings
+export const getOwnerBookings = async (req, res) => {
+    try {
+        if(req.user.role !== 'owner'){
+            return res.json({ success: false, message: "Not authorized" });
+        }
+        const {_id} = req.user;
+        const bookings = await Booking.find({owner: _id}).populate("car user").select("-user.password").sort({createdAt: -1});
+        res.json({ success: true, bookings });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+// API to update booking status
+export const updateBookingStatus = async (req, res) => {
+    try {
+        const {_id} = req.user;
+        const {bookingId, status} = req.body;
+        
+        const booking = await Booking.findById(bookingId);
+        if(booking.owner.toString() !== _id.toString()){
+            return res.json({ success: false, message: "Not authorized" });
+        }
+        booking.status = status;
+        await booking.save();
+        res.json({ success: true, message: "Booking status updated" });
+    } catch (error) {
+        
+    }
+}
