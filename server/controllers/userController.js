8af3ab8fb2852caacd638a8198a20a5fe250a908
Shubham Yadav <yadavshubham6.022@gmail.com>
2025-10-19 20:33:16 +0530

    booking schema is created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -131,3 +131,16 @@ export const deleteCar = async (req, res) => {
     }
 }
 
+// API to get Dashboard Data
+export const getDashboardData = async (req, res) => {
+    try{
+        const { _id, role } = req.user;
+        if(role !== 'owner'){
+            return res.json({ success: false, message: "Not authorized " });
+        }
+        const cars = await Car.find({ owner: _id });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -2,10 +2,14 @@ import express from 'express'
 import { protect } from '../middleware/auth.js';
 import { addCar, changeRoleToOwner } from '../controllers/ownerController.js';
 import upload from '../middleware/multer.js';
+import { deleteCar, getOwnerCars, toggleCarAvailability } from '../controllers/userController.js';
 
 const ownerRouter = express.Router();
 
 ownerRouter.post('/change-role', protect, changeRoleToOwner);
 ownerRouter.post('/add-car', upload.single('image'), protect, addCar);
+ownerRouter.get('/cars', protect, getOwnerCars);
+ownerRouter.post('/toggle-car', protect, toggleCarAvailability);
+ownerRouter.post('/delete-car', protect, deleteCar);
 
 export default ownerRouter;
