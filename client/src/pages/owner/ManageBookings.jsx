5a2ecdde09bdcaf50297bbf94dc15ebff1850646
Shubham Yadav <yadavshubham6.022@gmail.com>
2025-10-22 01:53:26 +0530

    complete owner dashboard with full CRUD functionality

--- client/src/pages/owner/ManageBookings.jsx
+++ client/src/pages/owner/ManageBookings.jsx
@@ -1,13 +1,40 @@
 import React, { useState } from "react";
 import { dummyMyBookingsData } from "../../assets/assets";
 import Title from "../../components/owner/Title";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const ManageBookings = () => {
-  const currency = import.meta.env.VITE_CURRENCY;
+  const { currency, axios } = useAppContext();
+
   const [bookings, setbookings] = useState([]);
+
   const fetchOwnerBookings = async () => {
-    setbookings(dummyMyBookingsData);
+    try {
+      const { data } = await axios.get("/api/bookings/owner-bookings");
+      data.success ? setbookings(data.bookings) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
+
+  const changeBookingStatus = async (bookingId, status) => {
+    try {
+      const { data } = await axios.post("/api/bookings/change-status",{
+        bookingId,
+        status
+      });
+      if(data.success){
+        toast.success(data.message);
+      }
+      else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   React.useEffect(() => {
     fetchOwnerBookings();
   }, []);
@@ -56,7 +83,7 @@ const ManageBookings = () => {
                 </td>
                 <td className="p-3">
                   {booking.status === "pending" ? (
-                    <select
+                    <select onChange={ e => {changeBookingStatus(booking._id, e.target.value)}}
                       value={booking.status}
                       className="px-2 py-1.5 mt-1 text-gray-500 border border-borderColor rounded-md outline-none"
                     >
